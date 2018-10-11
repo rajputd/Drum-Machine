d18b94e596830f2e698ba2696b214f235a27f5bc
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-11 18:45:46 -0400

    Modify SoundButton so it will play audio when it's given trigger key is pressed

--- src/components/SoundButton.js
+++ src/components/SoundButton.js
@@ -4,10 +4,30 @@ export default class SoundButton extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.playAudio = this.playAudio.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.handleKeyPress);
+  }
+
+  handleKeyPress(event) {
+    if (event.key == this.props.triggerKey) {
+      this.playAudio();
+    }
+  }
+
+  playAudio() {
+    this.props.audio.play();
   }
 
   handleClick() {
-    this.props.audio.play();
+    this.playAudio();
   }
 
   render() {
